6d75153e1e3d7718a1322fc4a7980961278553de
sdfnz <30536578+sdfnz@users.noreply.github.com>
2019-01-04 16:11:14 -0600

    Added basic drawing functionality

--- ink.js
+++ ink.js
@@ -1,11 +1,48 @@
-var canvas = document.getElementById("front");
-var ctx = canvas.getContext("2d");
+let canvas = document.getElementById("front");
+let context = canvas.getContext("2d");
 
 window.addEventListener('resize', resizeCanvas);
-window.addEventListener('mousedown', startDraw);
-window.addEventListener('mousemove', continueDraw);
-window.addEventListener('mouseup', endDraw);
-var paint = false;
+
+
+let isDrawing = false;
+let pointsX = new Array();
+let pointsY = new Array();
+
+let draw = function() {
+	function startDraw(e) {
+		context.strokeStyle = "blue";
+		context.lineJoin = "round";
+		context.lineCap = "round";
+		context.lineWidth = 15;
+		isDrawing = true;
+		context.beginPath();
+		let x = e.pageX - canvas.getBoundingClientRect().left;
+		let y = e.pageY - canvas.getBoundingClientRect().top;
+		pointsX.push(x);
+		pointsY.push(y);
+		context.moveTo(x, y);
+	}
+
+	function continueDraw(e) {
+		if (isDrawing) {
+			let x = e.pageX - canvas.getBoundingClientRect().left;
+			let y = e.pageY - canvas.getBoundingClientRect().top;
+			pointsX.push(x);
+			pointsY.push(y);
+			context.lineTo(x, y);
+			context.stroke();	
+		}
+	}
+
+	function stopDraw() {
+		isDrawing = false;
+	}
+
+	canvas.addEventListener("mousedown", startDraw);
+	canvas.addEventListener("mousemove", continueDraw);
+	canvas.addEventListener("mouseup", stopDraw);
+	canvas.addEventListener("mouseout", stopDraw);
+}
 
 function resizeCanvas() {
 	canvas.width = window.innerWidth;
@@ -13,16 +50,5 @@ function resizeCanvas() {
 }
 
 resizeCanvas()
-
-function startDraw() {
-	paint = true;
-}
-
-function continueDraw() {
-
-}
-
-function endDraw() {
-	paint = false;
-}
+draw();
 
