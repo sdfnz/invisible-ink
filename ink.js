a899cebbc30df21afbde4e5671e9c7dd9b481ab8
sdfnz <30536578+sdfnz@users.noreply.github.com>
2019-03-22 13:22:40 -0500

    Added buttons to clear the canvas and to reveal what was drawn. Finished base functionality.

--- ink.js
+++ ink.js
@@ -4,6 +4,8 @@ let context = canvas.getContext("2d");
 window.addEventListener('resize', resizeCanvas);
 
 let isDrawing = false;
+let revealed = false;
+let timedFade;
 let pointsX = new Array();
 let pointsY = new Array();
 
@@ -35,6 +37,8 @@ let draw = function() {
 
 	function stopDraw() {
 		isDrawing = false;
+		pointsX.push(-1);
+		pointsY.push(-1);
 	}
 
 	canvas.addEventListener("mousedown", startDraw);
@@ -46,13 +50,61 @@ let draw = function() {
 function fadeInk() {
 	context.fillStyle = "rgba(249,250,237,0.3)";
 	context.fillRect(0, 0, canvas.width, canvas.height);
-	setTimeout(fadeInk, 300);
+	timedFade = setTimeout(fadeInk, 300);
+}
+
+function clearCanvas() {
+	if (revealed) {
+		context.fillStyle = "#454545";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		pointsX = [];
+		pointsY = [];
+	} else {
+		context.fillStyle = "#f9faed";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		pointsX = [];
+		pointsY = [];
+	}
+}
+
+function revealInk() {
+	revealed = !revealed;
+	if (revealed) {
+		clearTimeout(timedFade);
+		context.fillStyle = "#454545";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		if (pointsX.length > 0) {
+			context.beginPath();
+			context.moveTo(pointsX[0], pointsY[0]);
+			for (let i = 0; i < pointsX.length; i++) {
+				if (pointsX[i] === -1) {
+					while (pointsX[i] === -1) {
+						i++;
+					}
+					context.beginPath();
+					context.moveTo(pointsX[i], pointsY[i]);
+				} else {
+					context.lineTo(pointsX[i], pointsY[i]);
+					context.stroke();	
+				}
+			}
+		}
+	} else {
+		context.fillStyle = "#f9faed";
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		fadeInk();
+	}
 }
 
 function resizeCanvas() {
 	canvas.width = window.innerWidth * .8;
 	canvas.height = window.innerHeight * .8;
 }
+
+const clearBtn = document.querySelector(".clear");
+const revealBtn = document.querySelector(".reveal");
+clearBtn.addEventListener("click", clearCanvas);
+revealBtn.addEventListener("click", revealInk);
 
 
 resizeCanvas()
